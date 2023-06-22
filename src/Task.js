872aae149eb233d0022cb1b12f489a3b83c2a7c8
erikaharmani <erikaa.nnmn@gmail.com>
2023-06-22 20:42:25 +0700

    initial commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Erika Nanda Harmani" // TODO: replace this
+const studentId = "FE5692381" // TODO: replace this
+
+export { url, studentName, studentId }
